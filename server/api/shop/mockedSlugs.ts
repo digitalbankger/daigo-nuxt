2c7b216e36d654a5a6918377114d97eb48e74e65
digitalbankger <bkmzen.bkmzen@mail.ru>
2025-12-09 16:24:42 +0300

    da

--- server/api/shop/mockedSlugs.ts
+++ server/api/shop/mockedSlugs.ts
@@ -1,7 +1,7 @@
 // server/api/shop/mockedSlugs.ts
 export const MOCKED_SLUGS = [
-  'tamotsu-and-daigo5ml',
-  'omega3-and-daigo5ml',
-  'brain-and-daigo5ml',
-  'frofilactica-orv',
+  'tamotsu-and-daigo5ml-t',
+  'omega3-and-daigo5ml-t',
+  'brain-and-daigo5ml-t',
+  'frofilactica-orv-t',
 ]
