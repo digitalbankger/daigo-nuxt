1a6705be41ecb6cd5a612d43fc9c626de348cf46
digitalbankger <bkmzen.bkmzen@mail.ru>
2026-05-28 21:20:31 +0300

    tech work

--- components/layout/HeadInformer.vue
+++ components/layout/HeadInformer.vue
@@ -4,27 +4,28 @@
        aria-label="Информер со ссылкой на каталог">
     <div class="relative flex items-center justify-center gap-4 px-3 sm:px-6 py-2 sm:py-2">
 
+      <span class="text-sm sm:text-base text-center font-mont font-medium py-1.5">Функционал сайта может быть недоступен<br class="sm:hidden"> с 00:00 до 04:00 МСК</span>
       <!-- DESKTOP TEXT -->
-      <NuxtLink
+      <!-- <NuxtLink
         to="/catalog"
         class="hidden sm:flex items-center gap-3 justify-center uppercase tracking-wide transition duration-300"
         @click="sendInformerGoal"
       >
         <span class="text-sm sm:text-lg font-mont font-medium">5% скидка на первый заказ</span>
-      </NuxtLink>
+      </NuxtLink> -->
 
       <!-- DESKTOP BUTTON -->
-      <button
+      <!-- <button
         type="button"
         class="hidden sm:inline-flex items-center justify-center gap-2 bg-[#9AFF9F] text-black rounded-lg py-1.5 px-4 text-sm uppercase transition hover:bg-[#7EFF7E] disabled:opacity-60 disabled:cursor-not-allowed"
         :disabled="busy"
         @click="applyWelcome()"
       >
         <span>{{ busy ? (isApplied ? 'Отмена…' : 'Применение…') : (isApplied ? 'Отменить WELCOME5' : 'Применить WELCOME5') }}</span>
-      </button>
+      </button> -->
 
       <!-- MOBILE -->
-      <div class="flex flex-col items-center gap-2 w-full justify-center sm:hidden uppercase">
+      <!-- <div class="flex flex-col items-center gap-2 w-full justify-center sm:hidden uppercase">
         <span class="text-xs sm:text-lg font-mont font-medium">5% скидка на первый заказ</span>
 
         <button
@@ -35,10 +36,10 @@
         >
           <span>{{ busy ? (isApplied ? 'Отмена…' : 'Применение…') : (isApplied ? 'Отменить WELCOME5' : 'Применить WELCOME5') }}</span>
         </button>
-      </div>
+      </div> -->
 
       <!-- CLOSE BUTTON -->
-      <button
+      <!-- <button
         type="button"
         class="absolute right-2 top-1/2 -translate-y-1/2 inline-flex items-center justify-center w-5 sm:w-7 h-5 sm:h-7 rounded-full text-black/90 hover:text-black/60 bg-white"
         aria-label="Скрыть информер"
@@ -47,7 +48,7 @@
         <svg viewBox="0 0 24 24" class="w-3 sm:w-4 h-3 sm:h-4" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M6 6L18 18M18 6L6 18" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
         </svg>
-      </button>
+      </button> -->
     </div>
   </div>
 </template>
