274002134b771cc7573de2cf1463d452155c2d89
digitalbankger <bkmzen.bkmzen@mail.ru>
2026-06-13 07:58:12 +0300

    Roistat

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -83,6 +83,29 @@ export default defineNuxtConfig({
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Sofia+Sans:wght@300;400;500;600;700;800;900&display=swap'
         }
+      ],
+      script: [
+        {
+        innerHTML: `
+(function(w, d, s, h, id) {
+    w.roistatProjectId = id;
+    w.roistatHost = h;
+    w.roistatPage = d.location.href;
+    w.roistatReferrer = d.referrer;
+    var p = d.location.protocol == "https:" ? "https://" : "http://";
+    var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie)
+      ? "/dist/module.js"
+      : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);
+    var js = d.createElement(s);
+    js.charset="UTF-8";
+    js.async = 1;
+    js.src = p+h+u;
+    var js2 = d.getElementsByTagName(s)[0];
+    js2.parentNode.insertBefore(js, js2);
+})(window, document, 'script', 'cloud.roistat.com', '8d6f3bc978e0a498604ac6d0377f7a8b');
+        `,
+        tagPosition: 'head'
+        },
       ]
     }
   },
