63e1997b696657d84c345c7873f0229edcdb10ad
digitalbankger <bkmzen.bkmzen@mail.ru>
2026-09-16 00:44:24 +0300

    Пререндер списка

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -192,8 +192,7 @@ export default defineNuxtConfig({
     prerender: {
       // /articles + каждый JSON из content/articles-json превращаются в статический HTML
       // во время pnpm build. Новый JSON автоматически попадёт в следующий деплой.
-      routes: [...articlePaginationPrerenderRoutes, ...articlePrerenderRoutes],
-      crawlLinks: false,
+      routes: [...articlePrerenderRoutes],      crawlLinks: false,
       failOnError: true,
     },
     // Локальные JSON-статьи включаются в production-сборку Nitro.
